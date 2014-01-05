67e9c8c6f39569f239250229b80e6f05f9b8f603
Dan Foster <dan@zem.org.uk>
2014-01-05 16:42:09 +0000

    base pelican plugin layout

--- picasa.py
+++ picasa.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 
 import gdata.photos.service
+from pelican import signals
 
-username = "dan@zem.org.uk" 
-albumname = "Gentse Feesten 2013"
-gd_client = gdata.photos.service.PhotosService()
-
-
-albums = gd_client.GetUserFeed(user=username)
-target = None
-for album in albums.entry:
-    if album.title.text == albumname:
-        target = album
-
-if not target:
-    print "ERROR: Cannot find album %s" % albumname
+def process_album():
+    username = "dan@zem.org.uk" 
+    albumname = "Gentse Feesten 2013"
+    gd_client = gdata.photos.service.PhotosService()
 
 
-photos = gd_client.GetFeed(
-        '/data/feed/api/user/%s/albumid/%s?kind=photo&imgmax=1024' % (
-        username, target.gphoto_id.text))
-for photo in photos.entry:
-    print 'Photo title:', photo.title.text, photo.content.src
-    print photo
-    print ""
+    albums = gd_client.GetUserFeed(user=username)
+    target = None
+    for album in albums.entry:
+        if album.title.text == albumname:
+            target = album
+
+    if not target:
+        print "ERROR: Cannot find album %s" % albumname
+
+
+    photos = gd_client.GetFeed(
+            '/data/feed/api/user/%s/albumid/%s?kind=photo&imgmax=1024' % (
+            username, target.gphoto_id.text))
+    for photo in photos.entry:
+        print 'Photo title:', photo.title.text, photo.content.src
+
+def add_picasa_article:
+    pass
+
+def add_picasa_page:
+    pass
+
+def register():
+    signals.article_generator_finalized.connect(add_picasa_article)
+    signals.article_generator_finalized.connect(add_picasa_page)
